c49f5123bad68324a859ff834197de5549c0e6f0
石琦峰 <byhieg@gmail.com>
2017-03-06 22:21:21 +0800

    完成生成树，并存储

--- src/com/cacheserverdeploy/deploy/Deploy.java
+++ src/com/cacheserverdeploy/deploy/Deploy.java
@@ -1,7 +1,9 @@
 package com.cacheserverdeploy.deploy;
 
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class Deploy {
@@ -24,16 +26,17 @@ public class Deploy {
     private static int deploymentCost;
 
     //网络链路图的带权图的邻接矩阵
-    private static Weight [][] graph;
+    private static Weight[][] graph;
 
     /*消费节点与相连网络节点的信息存储
       存储的格式如下：Map<Integer,ConsumptionNodeInfo>
       map中key是消费节点，value是相连的网络节点的ID与需要的带宽
      */
-    private static Map<Integer,ConsumptionNodeInfo> consumptionInfo;
+    private static Map<Integer, ConsumptionNodeInfo> consumptionInfo;
 
     //表示不可达的常亮
-    private static Weight NO_PATH_WEIGHT=new Weight();
+    private static Weight NO_PATH_WEIGHT = new Weight();
+
     static {
         NO_PATH_WEIGHT.setTotalBandwidth(0);
         NO_PATH_WEIGHT.setNetRentCost(101);
@@ -42,6 +45,22 @@ public class Deploy {
     public static String[] deployServer(String[] graphContent) {
         /**do your work here**/
         initData(graphContent);
+        Map<Integer, ArrayList<Integer>> map = createMST(0);
+        Iterator iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry entry = (Map.Entry) iterator.next();
+            ArrayList<Integer> lists = (ArrayList<Integer>) entry.getValue();
+            for (int i = 0; i < lists.size(); i++) {
+                System.out.println(entry.getKey() + " " + lists.get(i));
+            }
+//            System.out.print(entry.getKey());
+//            System.out.println("--------------");
+//            ArrayList<Integer> lists = (ArrayList<Integer>) entry.getValue();
+//            for (int i = 0 ; i < lists.size();i++) {
+//                System.out.print(lists.get(i) + " ");
+//            }
+//            System.out.println();
+        }
         return new String[]{"17", "\r\n", "0 8 0 20"};
     }
 
@@ -49,74 +68,94 @@ public class Deploy {
      * 生成最小生成树（以邻接矩阵存储）
      * 生成树的策略从一个点 看其相连的边，在边的全值中，选择 网络租用费用最低，流量最大。
      * 生成树的结构 由实现人决定
+     *
      * @param start
      * @return 返回生成树
      */
-    public static Weight[][] createMST(int start){
+    public static Map<Integer, ArrayList<Integer>> createMST(int start) {
 
         //保存当前生成树到到剩余各顶点最小的weight值
-        Weight[] lowcost=new Weight[netNodeCount];
+        Weight[] lowcost = new Weight[netNodeCount];
         //顶点i是否被并入生成树中
-        int[] vset=new int[netNodeCount];
+        int[] vset = new int[netNodeCount];
         //最小值
-        Weight min=new Weight();
+        Weight min = NO_PATH_WEIGHT;
         //保存新加入生成树的节点
-        int v=0;
+        int v = 0;
         //保存剩余顶点到当前生成树权值最小的边的顶点
-        int k=0;
-        //保存生成树的邻接矩阵
-        Weight[][] minTree=new Weight[netNodeCount][netNodeCount];
+        int k = 0;
+//        //保存生成树的邻接矩阵
+//        Weight[][] minTree = new Weight[netNodeCount][netNodeCount];
         //保存被并入生成树的节点的前驱节点（邻接点）
-        int[] preset=new int[netNodeCount];
+        int[] preset = new int[netNodeCount];
+
+        //
+        Map<Integer, ArrayList<Integer>> mst = new HashMap<>(netNodeCount);
 
         //初始化
-        for(int i=0;i<netNodeCount;i++){
-            lowcost[i]=graph[start][i];
-            vset[i]=0;
-            preset[i]=start;
+        for (int i = 0; i < netNodeCount; i++) {
+            lowcost[i] = graph[start][i];
+            vset[i] = 0;
+            preset[i] = start;
         }
         //起始节点被并入生成树
-        vset[start]=1;
+        vset[start] = 1;
 
-        for(int i=0;i<netNodeCount;i++){
-            //min初始化为比图中任何边的值都大的常量
-            min.setTotalBandwidth(0);
-            min.setNetRentCost(101);
+        for (int m = 0; m < netNodeCount; m++) {
+            ArrayList<Integer> a = new ArrayList();
+            mst.put(m, a);
+        }
+
+        for (int i = 0; i < netNodeCount; i++) {
+
+            min = NO_PATH_WEIGHT;
 
             //选出候选边中的最小者
-            for(int j=0;j<netNodeCount;j++){
-                if(vset[j]==0 && compareWeight(lowcost[j],min)){
-                    min=lowcost[j];
-                    k=j;
+            for (int j = 0; j < netNodeCount; j++)
+                if (vset[j] == 0 && compareWeight(lowcost[j], min)) {
+                    min = lowcost[j];
+                    k = j;
                 }
-                //k并入生成树
-                vset[k]=1;
-                //k设为中介节点
-                v=k;
-                //把边k和其前驱节点连接的边保存到生成树
-                minTree[preset[k]][k]=min;
+            //k并入生成树
+            vset[k] = 1;
+            //k设为中介节点
+            v = k;
+//                //把边k和其前驱节点连接的边保存到生成树
+//                minTree[preset[k]][k] = min;
 
-                //以刚并入的顶点v为中介，更新候选边和某些节点的前驱节点
-                for(int l=0;l<netNodeCount;l++){
+            mst.get(preset[v]).add(v);
 
-                    if(vset[l]==0 && compareWeight(graph[v][l],lowcost[l])){
-                        lowcost[j]=graph[v][l];
-                        preset[l]=v;
-                    }
+            //检查所有节点是否并入生成树中
+            int result = vset[0];
+            for (int n = 1; n < netNodeCount; n++) {
+                result &= vset[n];
+            }
+            if (result == 1) {
+                break;
+            }
 
+            //以刚并入的顶点v为中介，更新候选边和某些节点的前驱节点
+            for (int l = 0; l < netNodeCount; l++) {
+
+                if (vset[l] == 0 && compareWeight(graph[v][l], lowcost[l])) {
+                    lowcost[l] = graph[v][l];
+                    preset[l] = v;
                 }
 
             }
+
+
         }
-        return minTree;
+        return mst;
     }
 
     /**
      * 比较两个weight的大小
-     * @param两个weight
+     *
      * @return 前者比后者小，返回TRUE，否则返回FALSE
+     * @param两个weight
      */
-    public static boolean compareWeight(Weight weight1,Weight weight2){
+    public static boolean compareWeight(Weight weight1, Weight weight2) {
 
 //        if(weight1==null && weight2==null) return false;
 //        else if(weight1==null){
@@ -125,10 +164,10 @@ public class Deploy {
 //            return true;
 //        }
 
-        if(weight1.getNetRentCost()<weight2.getNetRentCost()) return true;
-        else if(weight1.getNetRentCost()>weight2.getNetRentCost()) return false;
+        if (weight1.getNetRentCost() < weight2.getNetRentCost()) return true;
+        else if (weight1.getNetRentCost() > weight2.getNetRentCost()) return false;
         else {
-            if(weight1.getTotalBandwidth()>weight2.getTotalBandwidth()) return true;
+            if (weight1.getTotalBandwidth() > weight2.getTotalBandwidth()) return true;
         }
 
         return false;
@@ -141,10 +180,11 @@ public class Deploy {
      * 3. 判断该服务器是否能满足所有的消费节点
      * 4. 如果能全部满足，在添加第二个服务器，添加的规则是在第二个度最大的添加，判断第二个服务器成本是否降低
      * 5. 如果不能全部满足，在添加第二个服务器，判断是否满足全部节点，如果不满足，则依次添加
+     *
      * @param mst 一个生成树类型的变量，不是String
      * @return 所有服务器的ID
      */
-    public static int[] getDeployment(String mst){
+    public static int[] getDeployment(String mst) {
 
         return new int[]{};
     }
@@ -156,38 +196,36 @@ public class Deploy {
      * 4. 如果不能全部走到，保存不能走到的消费节点，假设不加服务器，加边()
      * 5. 加边的策略：消费节点所需带宽-最小生成树中的边的带宽 走费用最小的带宽最大的，从下往上走
      *
-     *
-     *
      * @param serverID 一组服务器的ID
      * @return true 如果部署的服务器能满足所有的消费节点。false 不满足
      */
-    public static boolean isSatisfyAllConsumptionNode(int[] serverID){
+    public static boolean isSatisfyAllConsumptionNode(int[] serverID) {
         return false;
     }
 
 
-
     /**
      * 处理图文本信息，构造图
+     *
      * @param graphContent 图文本信息
      */
     private static void initData(String[] graphContent) {
 
         //处理网络节点数量,成本等信息
-        String [] firstLine = lineTos(graphContent[0]);
+        String[] firstLine = lineTos(graphContent[0]);
         netNodeCount = sToi(firstLine[0]);
         netLinkCount = sToi(firstLine[1]);
         consumptionNodeCount = sToi(firstLine[2]);
 
-        String [] thirdLine = lineTos(graphContent[2]);
+        String[] thirdLine = lineTos(graphContent[2]);
         deploymentCost = sToi(thirdLine[0]);
 
         //构造图
         graph = new Weight[netNodeCount][netNodeCount];
 
         //初始化图中的权值信息，默认为所有节点不可达
-        for (int i = 0 ; i < netNodeCount; i ++) {
-            for (int j = 0 ; j < netNodeCount;j++) {
+        for (int i = 0; i < netNodeCount; i++) {
+            for (int j = 0; j < netNodeCount; j++) {
                 graph[i][j] = NO_PATH_WEIGHT;
             }
         }
@@ -198,7 +236,7 @@ public class Deploy {
         int lineNum = 4;
 
         while (isNotEnd) {
-            String [] edgeInfo = lineTos(graphContent[lineNum++]);
+            String[] edgeInfo = lineTos(graphContent[lineNum++]);
             //设置起点
             int originID = sToi(edgeInfo[0]);
             //设置终点
@@ -232,16 +270,15 @@ public class Deploy {
             ConsumptionNodeInfo info = new ConsumptionNodeInfo();
             info.setLinkedID(linkedID);
             info.setRequiredBandWidth(requiredBandWidth);
-            consumptionInfo.put(consumptionID,info);
+            consumptionInfo.put(consumptionID, info);
         }
 
     }
 
 
-
-
     /**
-     *字符串转换成整数
+     * 字符串转换成整数
+     *
      * @param s 字符串
      * @return i 转换成整数
      */
@@ -251,6 +288,7 @@ public class Deploy {
 
     /**
      * 一行的字符串分割成一个字符串数组
+     *
      * @param line 一行的字符串
      * @return 分割后的字符串数组
      */
@@ -266,7 +304,7 @@ public class Deploy {
      * 网络节点链路图的类
      * 图中边的权值信息，包括总带宽费用，网络租用费
      */
-    public static class Weight{
+    public static class Weight {
         //总带宽费用
         private int totalBandwidth;
         //网络租用费
@@ -304,7 +342,7 @@ public class Deploy {
      * 与之相连的ID
      * 需求的带宽
      */
-    private static class ConsumptionNodeInfo{
+    private static class ConsumptionNodeInfo {
         //相连的ID
         private int linkedID;
         //需求的带宽
